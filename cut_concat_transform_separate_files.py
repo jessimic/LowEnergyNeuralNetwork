0e2f0ba5637a5b5c2623577836b4e6afd0b0d383
jessimic <micall12@msu.edu>
2020-05-22 14:44:11 -0400

    tightened memory usage

--- cut_concat_transform_separate_files.py
+++ cut_concat_transform_separate_files.py
@@ -18,14 +18,17 @@ import numpy as np
 import glob
 import h5py
 import argparse
+import psutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input_files",default=None,
                     type=str,dest="input_files", help="names for input files")
 parser.add_argument("-d", "--path",type=str,default='/mnt/scratch/micall12/training_files/',
                     dest="path", help="path to input files")
-parser.add_argument("-o", "--output",type=str,default='cut_concat_separated',
-                    dest="output", help="names for output files")
+parser.add_argument("-o", "--outdir",type=str,default='/mnt/scratch/micall12/training_files/',
+                    dest="outdir", help="path for storing output file")
+parser.add_argument("-n", "--name",type=str,default='cut_concat_separated',
+                    dest="name", help="name for output file")
 parser.add_argument("-c", "--cuts", type=str, default="all",
                     dest="cuts", help="name of cuts applied (see name options on line 38)")
 # cut names: track, cascade, CC, NC, track CC, track NC, cascade CC, cascade NC 
@@ -35,6 +38,8 @@ parser.add_argument("--num_out",type=int,default=1,
                     dest="num_out",help="number of output files you want to split the output into")
 parser.add_argument("--emax",type=float,default=200.0,
                     dest="emax",help="Max energy to keep, cut anything above")
+parser.add_argument("--emin",type=float,default=5.0,
+                    dest="emin",help="Min energy to keep, cut anything below")
 parser.add_argument("--shuffle",type=str, default=True,
                     dest="shuffle", help="False if you don't want to shuffle")
 parser.add_argument("--find_statistics",type=str, default=False,
@@ -50,12 +55,14 @@ parser.add_argument("--tmax",type=float,default=None,
 args = parser.parse_args()
 input_files = args.input_files
 path = args.path
-output = args.output
+outdir = args.outdir
+output = args.name
 num_outputs = args.num_out
 assert num_outputs<100, "NEED TO CHANGE FILENAME TO ACCOMODATE THIS MANY NUMBERS"
 transform = args.scaler
 cut_name = args.cuts
 emax = args.emax
+emin = args.emin
 max_energy = emax
 max_track = args.tmax
 find_statistics = args.statistics
@@ -110,8 +117,11 @@ full_labels = None
 full_reco = None
 full_initial_stats = None
 full_num_pulses = None
+full_trig_times = None
 
 # Labels: [ nu energy, nu zenith, nu azimuth, nu time, nu x, nu y, nu z, track length (0 for cascade), isTrack (track = 1, cascasde = 0), flavor, type (anti = 1), isCC (CC=1, NC = 0)]
+
+print(psutil.virtual_memory()[1] >> 30) 
 
 for a_file in event_file_names:
 
@@ -121,8 +131,6 @@ for a_file in event_file_names:
     file_labels = f["labels"][:]
     if use_old_reco:
         file_reco = f["reco_labels"][:]
-        file_initial_stats = f["initial_stats"][:]
-        file_num_pulses = f["num_pulses_per_dom"][:]
     f.close()
     del f
 
@@ -132,7 +140,7 @@ for a_file in event_file_names:
    
     from handle_data import CutMask
     mask = CutMask(file_labels)
-    e_mask = np.array(file_labels[:,0])<emax
+    e_mask = np.logical_and(np.array(file_labels[:,0])>emin, np.array(file_labels[:,0])<emax)
     keep_index = np.logical_and(mask[cut_name],e_mask)
     number_events = sum(keep_index)
 
@@ -157,45 +165,37 @@ for a_file in event_file_names:
         else:
             full_reco = np.concatenate((full_reco, file_reco[keep_index]))
 
-        if full_initial_stats is None:
-            full_initial_stats = file_initial_stats[keep_index]
-        else:
-            full_initial_stats  = np.concatenate((full_initial_stats , file_initial_stats[keep_index]))
-
-        if full_num_pulses is None:
-            full_num_pulses = file_num_pulses[keep_index]
-        else:
-            full_num_pulses = np.concatenate((full_num_pulses, file_num_pulses[keep_index]))
-        
-
     print("Events this file: %i, Saved this file: %i, Cumulative saved: %i\n Finsihed file: %s"%(number_events,np.count_nonzero(keep_index),full_labels.shape[0],a_file))
+del file_features_DC
+del file_features_IC
+del file_labels
+if use_old_reco:
+    del file_reco
+print(psutil.virtual_memory()[1] >> 30) 
 
 if shuffle:
     print("Finished concatonating all the files. Now I will shuffle..")
     from handle_data import Shuffler
 
-    shuffled_features_DC, shuffled_features_IC, shuffled_labels, \
-    shuffled_reco, shuffled_initial_stats, shuffled_num_pulses,shuffled_trig_times = \
+    full_features_DC, full_features_IC, full_labels, \
+    full_reco, full_initial_stats, full_num_pulses,full_trig_times = \
     Shuffler(full_features_DC,full_features_IC,full_labels,\
     full_reco=full_reco, full_initial_stats=full_initial_stats,\
     full_num_pulses=full_num_pulses,use_old_reco_flag=use_old_reco)
-else: 
-    shuffled_features_DC, shuffled_features_IC, shuffled_labels, \
-    shuffled_reco, shuffled_initial_stats, shuffled_num_pulses,shuffled_trig_times = \
-    full_features_DC,full_features_IC,full_labels,\
-    full_reco, full_initial_stats,full_num_pulses
-
+print(psutil.virtual_memory()[1] >> 30)
 
 #Transform Input Data
 from scaler_transformations import TransformData, new_transform
 
-features_DC_partial_transform = new_transform(shuffled_features_DC)
-features_DC_full_transform = TransformData(features_DC_partial_transform, low_stats=low_stat_DC, high_stats=high_stat_DC, scaler=transform)
+full_features_DC = new_transform(full_features_DC)
+full_features_DC = TransformData(full_features_DC, low_stats=low_stat_DC, high_stats=high_stat_DC, scaler=transform)
 print("Finished DC")
+print(psutil.virtual_memory()[1] >> 30)
 
-features_IC_partial_transform = new_transform(shuffled_features_IC)
-features_IC_full_transform = TransformData(features_IC_partial_transform, low_stats=low_stat_IC, high_stats=high_stat_IC, scaler=transform)
+full_features_IC = new_transform(full_features_IC)
+full_features_IC = TransformData(full_features_IC, low_stats=low_stat_IC, high_stats=high_stat_IC, scaler=transform)
 print("Finished IC")
+print(psutil.virtual_memory()[1] >> 30)
 
 print("Finished transforming the data using %s Scaler"%transform)
 
@@ -206,23 +206,22 @@ print("Finished transforming the data using %s Scaler"%transform)
 if transform_output:
     if not max_energy:
         print("Not given max energy, finding it from the Y_test in the given file!!!")
-        max_energy = max(abs(shuffled_labels[:,0]))
+        max_energy = max(abs(full_labels[:,0]))
     if not max_track:
         print("Not given max track, finding it from the Y_test in the given file!!!")
-        max_track = max(shuffled_labels[:,7])
+        max_track = max(full_labels[:,7])
 
-    labels_transform = np.copy(shuffled_labels)
-
-    labels_transform[:,0] = shuffled_labels[:,0]/float(max_energy) #energy
-    labels_transform[:,1] = np.cos(shuffled_labels[:,1]) #cos zenith
-    labels_transform[:,2] = shuffled_labels[:,7]/float(max_track) #MAKE TRACK THIRD INPUT
-    labels_transform[:,7] = shuffled_labels[:,2] #MOVE AZIMUTH TO WHERE TRACK WAS
+    # switch track and azimuth positions
+    track = full_labels[:,7]
+    azimuth = full_labels[:,2]
+    
+    full_labels[:,0] = full_labels[:,0]/float(max_energy) #energy
+    full_labels[:,1] = np.cos(full_labels[:,1]) #cos zenith
+    full_labels[:,2] = track/float(max_track) #MAKE TRACK THIRD INPUT
+    full_labels[:,7] = azimuth #MOVE AZIMUTH TO WHERE TRACK WAS
 
     print("Transforming the energy and zenith output. Dividing energy by %f and taking cosine of zenith"%max_energy)
     print("Transforming track output. Dividing track by %f and MOVING IT TO INDEX 2 IN ARRAY. AZIMUTH NOW AT 7"%max_track)
-else:
-    labels_transform = np.array(shuffled_labels)
-
 
 #Split data
 from handle_data import SplitTrainTest
@@ -230,13 +229,13 @@ X_train_DC, X_train_IC, Y_train, \
 X_test_DC, X_test_IC, Y_test, \
 X_validate_DC, X_validate_IC, Y_validate,\
 reco_train, reco_test, reco_validate  \
-= SplitTrainTest(features_DC_full_transform,features_IC_full_transform,labels_transform,\
-reco=shuffled_reco,use_old_reco=use_old_reco,create_validation=create_validation,\
+= SplitTrainTest(full_features_DC,full_features_IC,full_labels,\
+reco=full_reco,use_old_reco=use_old_reco,create_validation=create_validation,\
 fraction_test=0.1,fraction_validate=0.2)
 
 
 #Save output to hdf5 file
-print("Total events saved: %i"%features_DC_full_transform.shape[0])
+print("Total events saving: %i"%full_features_DC.shape[0])
 
 #Save output to hdf5 file
 cut_name_nospaces = cut_name.replace(" ","")
@@ -252,7 +251,7 @@ test_per_file = int(X_test_DC.shape[0]/num_outputs) + 1
 validate_per_file = int(X_validate_DC.shape[0]/num_outputs) + 1
 print("Saving %i train events, %i test events, and %i validate events per %i file(s)"%(train_per_file,test_per_file,validate_per_file,num_outputs))
 for sep_file in range(0,num_outputs):
-    output_file = "/mnt/scratch/micall12/training_files/" + output + cut_file_name + transform_name + "_file%02d.hdf5"%sep_file
+    output_file = outdir + output + cut_file_name + transform_name + "_file%02d.hdf5"%sep_file
 
     train_start = train_per_file*sep_file
     test_start = test_per_file*sep_file
