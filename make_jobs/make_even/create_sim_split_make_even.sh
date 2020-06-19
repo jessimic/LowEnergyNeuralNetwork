b4d768692fdd2d059a0030c0b1905f6a69b582fc
jessimic <micall12@msu.edu>
2020-06-19 13:57:54 -0400

    example job scripts for making flat sample

--- make_jobs/make_even/create_sim_split_make_even.sh
+++ make_jobs/make_even/create_sim_split_make_even.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-FLV="E"
-NUM=12
-BIN_MAX=10000
+FLV="Mu"
+NUM=14
+BIN_MAX=36499
+VERTEX="start_IC7"
 
-for SIM in 2 3; #4 6;
-do
-    infile=Nu${FLV}_${NUM}0000_00????_level2_sim${SIM}.zst_lt200_NOvertex_IC19.hdf5
-    name=Nu${FLV}_${NUM}0000_level2_sim${SIM}_IC19
-    sed -e "s|@@infile@@|${infile}|g" \
-        -e "s|@@bin_max@@|$BIN_MAX|g" \
-        -e "s|@@name@@|${name}|g" \
-        < job_template_make_even_simX.sb > make_even_Nu${FLV}_sim${SIM}.sb
+for VERTEX in "start_IC7" "start_DC";
+    do
+    for SIM in 2 3 4 6 7 8;
+    do
+        infile=Nu${FLV}_${NUM}0000_00????_level2_sim${SIM}.zst_lt200_NOvertex_IC19.hdf5
+        name=Nu${FLV}_${NUM}0000_level2_sim${SIM}_IC19_${VERTEX}
+        sed -e "s|@@infile@@|${infile}|g" \
+            -e "s|@@bin_max@@|$BIN_MAX|g" \
+            -e "s|@@name@@|${name}|g" \
+            -e "s|@@vertex@@|${VERTEX}|g" \
+            < job_template_make_even_simX.sb > make_even_Nu${FLV}_sim${SIM}_${VERTEX}.sb
+    done
 done
 
