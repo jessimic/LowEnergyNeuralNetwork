20dd22075232ca66188ba7bc768edb14800c8335
jessimic <micall12@msu.edu>
2021-02-10 17:15:08 -0500

    MAJOR updated, minus all the extra log files & job scripts

--- make_jobs/i3_test/slurm/run_all_here.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#FILES="oscNext_genie_level7_v02.00_pass2.120000.000???.i3.zst.sb"
-FILES="Nu*_genie_1?9999_000[4,5,6,7,8,9]??_level6.zst.sb"
-
-for f in $FILES
-do
-    sbatch $f
-done
